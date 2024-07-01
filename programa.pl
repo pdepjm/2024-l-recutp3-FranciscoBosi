01d21c5e57f700e404ee664263b217908fba527c
FranciscoBosi <83473717+FranciscoBosi@users.noreply.github.com>
2024-07-01 15:30:49 -0300

    Update programa.pl

--- programa.pl
+++ programa.pl
@@ -1,1 +1,33 @@
-% Aquí va el código.
+
+%trabajaEn(persona,puesto,departamento).
+
+trabajaEn(maria,empleado,ventas).
+trabajaEn(juan,cadete,ventas).
+trabajaEn(roque,pasante,ventas).
+trabajaEn(nora,empleado,compras).
+trabajaEn(pedro,cadete,compras).
+trabajaEn(felipe,empleado,admininstracion).
+trabajaEn(hugo,empleado,administracion).
+trabajaEn(ana,cadete,administracion).
+
+puedeDarOrdenes(Persona1,Persona2):-
+    trabajaEnMismoDepa(Persona1,Persona2),
+    tienePuestoSuperior(Persona1,Persona2).
+
+trabajaEnMismoDepa(Persona1,Persona2):-
+    trabajaEn(Persona1,_,Depa1),
+    trabajaEn(Persona2,_,Depa2),
+    Depa1==Depa2.
+
+tienePuestoSuperior(Persona1,Persona2):-
+    trabajaEn(Persona1,empleado,_),
+    trabajaEn(Persona2,cadete,_).
+
+    tienePuestoSuperior(Persona1,Persona2):-
+        trabajaEn(Persona1,empleado,_),
+        trabajaEn(Persona2,pasante,_).
+        
+        tienePuestoSuperior(Persona1,Persona2):-
+            trabajaEn(Persona1,cadete,_),
+            trabajaEn(Persona2,pasante,_).
+
